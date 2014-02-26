311632bc89ae0c8cb10b515235c34e8f49ed7b07
Alexandru Onea <alexandru-nicolae.onea@student.manchester.ac.uk>
2014-02-26 00:02:52 +0000

    looks great

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,7 +1,21 @@
 $('a span').click(function(){
-	console.log($(this).parent()[0]);
+	console.log($.attr($(this).parent()[0], 'href'));
     $('html, body').animate({
-        scrollTop: $( $.attr($(this).parent()[0], 'href') ).offset().top
+        scrollTop: $($.attr($(this).parent()[0], 'href')).offset().top - 100
     }, 300);
     return false;
 });
+
+var checkScroll = function (e) {
+	if (window.scrollY >= 600) {
+		document.querySelector('nav').style.position = 'fixed';
+		document.querySelector('.nav-wrapper').style.position = 'fixed';
+		document.querySelector('.nav-wrapper').style.opacity = '.95';
+	} else {
+		document.querySelector('nav').style.position = 'absolute';
+		document.querySelector('.nav-wrapper').style.position = 'absolute';
+		document.querySelector('.nav-wrapper').style.opacity = '.3';
+	}
+};
+
+window.addEventListener('scroll', checkScroll, false);
