17f6890b4fe723dcea6b06d56ce8334054dae9d9
Alexandru Onea <onea.alex@gmail.com>
2014-02-27 19:47:15 +0000

    added speakers partial

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -6,6 +6,15 @@ $('nav ul li a, nav ul li span').click(function(e){
     return false;
 });
 
+$('.cover').click(function (e) {
+	var description = document.createElement('div');
+	description.classList.add('description');
+
+	this.parentNode.parentNode.parentNode.insertBefore(description, this.parentNode.parentNode.nextSibling);
+
+	description.classList.add('description-final');
+});
+
 var checkScroll = function (e) {
 	if (window.scrollY >= 600) {
 		document.querySelector('nav').style.position = 'fixed';
